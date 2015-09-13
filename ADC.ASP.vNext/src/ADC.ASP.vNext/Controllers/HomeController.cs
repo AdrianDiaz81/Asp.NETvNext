64fe0f5a45d2f26c5ec1b395e6ae9a773974169b
Adrian Diaz Cervera <ardydiaz@hotmail.com>
2015-09-13 12:47:04 +0200

    Subir Test

---
+++ ADC.ASP.vNext/src/ADC.ASP.vNext/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Configuration;
+
+namespace ADC.ASP.vNext.Controllers
+{
+    public class HomeController : Controller
+    {
+        private  IConfiguration _config;
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description pagina.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
